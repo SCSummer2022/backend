203ca3f36e92197e66f5fac74b4199a2fc059c08
tkuvshinov <79188990+tkuvshinov@users.noreply.github.com>
2022-07-05 15:36:54 +0300

    use -> get

--- test/hello.test.js
+++ test/hello.test.js
@@ -1,7 +1,8 @@
 const request = require('supertest')
 const express = require("express")
 const app = express()
-app.use('/', require('../api/hello'));
+
+app.get('/', require('../api/hello'))
 
 it('api\\hello.js', (done) => {
     request(app).get('/').expect({
