0e0dd6730b2e59d499b5a9dfc0b8548efee530b3
Doraxxx <ryumina.darya28@gmail.com>
2022-07-06 06:32:13 +0300

    Revert "Commit by Darya"
    This reverts commit d75d1de3b54855138b4d955af4889612cd596e6c.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = 3000
 app.post('/', require('./api/user'))
 app.get('/', require('./api/user'))
 app.delete('./', require('./api/user'))
-app.put('./', require('./api/user'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
