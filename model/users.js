d9661231064e4f7108731f50c10ccce65c4867b3
Doraxxx <ryumina.darya28@gmail.com>
2022-07-06 16:00:37 +0300

    +User

--- model/users.js
+++ model/users.js
@@ -1,16 +1,28 @@
 const sequelize = require('./db');
 
 const User = sequelize.sequelize.define('User', {
-    id: {type: 'STRING', primaryKey: true},
-    name: 'STRING',
-    code: 'STRING'
+    id: {type: 'STRING', primaryKey: true, allowNull: false, autoIncrement: true},
+    role: {type: 'STRING', allowNull: false},
+    last_name: {type: 'STRING', allowNull: false},
+    first_name: {type: 'STRING', allowNull: false},
+    second_name: {type: 'STRING', allowNull: false},
+    birthday: {type: 'DATE', allowNull: false},
+    phone_number: {type: 'INTEGER'},
+    email: {type: 'STRING', allowNull: false},
+    password: {type: 'STRING', allowNull: false}
+
 });
 
 User.sync()
     .then(() => User.create({
-            id: 'some_id',
-            name: 'some_name',
-            code: 'some_code'
+        role: 'администратор',
+        last_name: 'Андреевич',
+        first_name: 'Павел',
+        second_name: 'Небойков',
+        birthday: new Date(2002, 6, 20),
+        phone_number: '546789',
+        email: 'neboi222@mail.ru',
+        password: 'easypeasy'
         })
     )
 
