c6d081d1cac758cd2bc0c9f458cce2b93601adf5
asmirnov <asmirnov@solit-clouds.ru>
2022-07-06 17:02:53 +0300

    fix types

--- model/tournament.js
+++ model/tournament.js
@@ -1,17 +1,18 @@
 const sequelize = require('./db');
+const {DataTypes} = require('sequelize');
 
 const TournamentType = sequelize.sequelize.define('TournamentType', {
-    id: {type: 'STRING', primaryKey: true},
+    id: {type: DataTypes.STRING, primaryKey: true},
     name: 'STRING',
     code: 'STRING'
 });
 TournamentType.sync()
     .then(() => TournamentType.create({
-        id: 'some_id',
-        name: 'some_name',
-        code: 'some_code'
-    })
-)
+            id: 'some_id',
+            name: 'some_name',
+            code: 'some_code'
+        })
+    )
 
 const SportType = sequelize.sequelize.define('SportType', {
     id: {type: 'STRING', primaryKey: true},
@@ -20,11 +21,11 @@ const SportType = sequelize.sequelize.define('SportType', {
 });
 SportType.sync()
     .then(() => SportType.create({
-        id: 'some_id',
-        name: 'some_name',
-        code: 'some_code'
-    })
-)
+            id: 'some_id',
+            name: 'some_name',
+            code: 'some_code'
+        })
+    )
 
 const City = sequelize.sequelize.define('City', {
     id: {type: 'STRING', primaryKey: true},
@@ -32,45 +33,45 @@ const City = sequelize.sequelize.define('City', {
 });
 City.sync()
     .then(() => City.create({
-        id: 'some_id',
-        name: 'some_name'
-    })
-)
+            id: 'some_id',
+            name: 'some_name'
+        })
+    )
 
 const Tournament = sequelize.sequelize.define('Tournament', {
     id: {type: 'STRING', primaryKey: true},
     name: 'STRING',
     tournament_type_id: 'STRING',
-    sport_type : 'STRING',
-    start_date : 'DATE',
-    end_date : 'DATE',
-    school_or_city : 'BOOLEAN',
-    city : 'STRING',
-    participants_min : 'INTEGER',
-    participants_max : 'INTEGER',
-    age_min : 'INTEGER',
-    age_max : 'INTEGER',
-    class_min : 'INTEGER',
-    class_max : 'INTEGER',
-    team_size : 'INTEGER'
+    sport_type: 'STRING',
+    start_date: 'DATE',
+    end_date: 'DATE',
+    school_or_city: 'BOOLEAN',
+    city: 'STRING',
+    participants_min: 'INTEGER',
+    participants_max: 'INTEGER',
+    age_min: 'INTEGER',
+    age_max: 'INTEGER',
+    class_min: 'INTEGER',
+    class_max: 'INTEGER',
+    team_size: 'INTEGER'
 });
 Tournament.sync()
     .then(() => Tournament.create({
             id: 'some_id',
             name: 'some_name',
             tournament_type_id: 'some_id',
-            sport_type : 'some_id',
-            start_date : new Date(2022, 6, 20),
-            end_date : new Date(2022, 7, 21),
-            school_or_city : true,
-            city : 'some_city',
-            participants_min : 20,
-            participants_max : 23,
-            age_min : 12,
-            age_max : 15,
-            class_min : 7,
-            class_max : 8,
-            team_size : 10
+            sport_type: 'some_id',
+            start_date: new Date(2022, 6, 20),
+            end_date: new Date(2022, 7, 21),
+            school_or_city: true,
+            city: 'some_city',
+            participants_min: 20,
+            participants_max: 23,
+            age_min: 12,
+            age_max: 15,
+            class_min: 7,
+            class_max: 8,
+            team_size: 10
         })
     )
 
--- model/users.js
+++ model/users.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize/types');
+const { DataTypes } = require('sequelize');
 const sequelize = require('./db');
 
 const User = sequelize.sequelize.define('User', {
