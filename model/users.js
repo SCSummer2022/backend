07726c15bc326ed7abc7de00a67ed82936f1a297
HorryMode <dima-koshkin-2001@list.ru>
2022-07-06 16:04:16 +0300

    Да

--- model/users.js
+++ model/users.js
@@ -25,7 +25,46 @@ User.sync()
             password: 'easypeasy'
         })
     )
-
-
+const Role = sequelize.sequelize.define('Teams', {
+    id:{type: DataTypes.STRING, primaryKey: true, autoIncrement: true},
+        name: {type: DataTypes.STRING, allowNull: false}
+    });
+Role.sync()
+    .then(() => Role.create({
+        id: 'some_id',
+        name: 'some_name'
+    })
+    )
+const Teams = sequelize.sequelize.define('Teams', {
+    id:{type: DataTypes.STRING, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false}
+    });
+Teams.sync()
+    .then(() => Teams.create({
+        id: 'some_id',
+        name: 'some_name'
+    })
+)
+const School = sequelize.sequelize.define('Teams', {
+    id:{type: DataTypes.STRING, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false}
+    });
+School.sync()
+    .then(() => School.create({
+        id: 'some_id',
+        name: 'some_name'
+    })
+)
+const City = sequelize.sequelize.define('Teams', {
+    id:{type: DataTypes.STRING, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false}
+    });
+City.sync()
+    .then(() => City.create({
+        id: 'some_id',
+        name: 'some_name'
+    })
+)
 module.exports = {
     User: User
+}
