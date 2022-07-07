9783f6238922afdef0cb88576985d9913dcaf7ed
asmirnov <asmirnov@solit-clouds.ru>
2022-07-07 14:29:29 +0300

    fix test

--- test/match-controller.test.js
+++ test/match-controller.test.js
@@ -6,20 +6,21 @@ let service = require('../service/match');
 
 app.put('/tournament/:tournament_id/match/:match_id', require('../api/match'))
 
-it('PUT', function (done) {
+it('PUT', async function () {
     let tournamentId = 4
     let matchId = 3
     let matchChanges = {
-        "name" : "Матч 2 турнира 1 новое имя 2"
+        "name": "Матч 2 турнира 1 новое имя 2"
     }
-    request(app)
+    let response = await request(app)
         .put('/tournament/' + tournamentId + '/match/' + matchId + '/')
         .send(matchChanges)
-        .expect(async function (res) {
-            console.log('before request')
-            let TargetMatch = await service.findMatch(tournamentId, matchId)
-            await console.log('after request')
 
-        })
-        .end(done);
+    console.log('before request')
+    let TargetMatch = await service.findMatch(tournamentId, matchId)
+    await console.log('after request')
+
+    assert.ok(true)
+
+
 });
